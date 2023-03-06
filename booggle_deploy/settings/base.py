420c388517e8e2b0db6f1e114e480890a825d30e
Andy <andythomas.py@gmail.com>
2023-03-06 00:11:00 -0600

    Added login/logout

--- booggle_deploy/settings/base.py
+++ booggle_deploy/settings/base.py
@@ -17,6 +17,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -81,3 +83,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = 'static'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
