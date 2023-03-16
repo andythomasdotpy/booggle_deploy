9a56c86c11454366fc43e48c715da0dae4d8adea
Andy <andythomas.py@gmail.com>
2023-03-15 22:31:03 -0500

    Added phase 1 spinner

--- valuator/static/valuator/js/main.js
+++ valuator/static/valuator/js/main.js
@@ -1,7 +1,27 @@
-function showLoaderOnClick(url) {
-    showLoader();
-    window.location=url;
-}
-function showLoader(){
-    $('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+// function showLoaderOnClick(url) {
+//     showLoader();
+//     window.location=url;
+// }
+// function showLoader(){
+//     $('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+// }
+
+window.addEventListener("load", (event) => {
+    console.log("page is fully loaded");
+    document.querySelector('.mySpinnerDiv').classList.add('hidden')
+    document.querySelector('.section-formatting').classList.toggle('hidden')
+  });
+
+document.querySelector('.aTagButtonDiv').addEventListener('click', error)
+
+function error() {
+    const errorMsg = "Sorry, that functionality is currently under construction"
+
+    document.querySelector(".errorMsg").innerText = errorMsg
+
+
+    const showMessage = setTimeout(myMessage, 1500)
+    function myMessage() {
+        document.querySelector(".errorMsg").innerText = ""
+    }
 }
